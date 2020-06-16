cd11ca00158b97050f3ae56824b89e2c8d46d167
ouhouh1936 <ouhouh1936@gamil.com>
2020-06-16 21:58:00 +0900

    3

--- src/screens/ACSC01.js
+++ src/screens/ACSC01.js
@@ -2,11 +2,39 @@ import React from "react";
 import Lnb from "../components/Lnb";
 
 class ACSC01 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataList: [],
+    };
+  }
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      const arr = ["경민", "예림", "효진"];
+
+      this.setState({ dataList: arr });
+    }, 3000);
+  };
   render() {
+    const { dataList } = this.state;
+
     return (
       <div>
-        <div>
+        <div className="ASCS01">
           <Lnb title="About" column="HOME > Abouts" />
+          {dataList.length === 0 ? (
+            <div>데이터를 불러오는 중입니다 잠시만 기다려주세요 ... </div>
+          ) : (
+            dataList.map((data, idx) => {
+              return (
+                <div>
+                  {idx + 1}.{data}
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
     );
